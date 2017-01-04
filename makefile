e97cb9a811566cb67be4dd62379d840a34c545e5
Steve Cirelli <scirelli@gmail.com>
2017-01-04 01:50:36 +0000

    Updates

--- makefile
+++ makefile
@@ -5,3 +5,5 @@ debug:
 deploy: all
 	sudo mv switch_c.cgi /var/www/html/cgi-bin/
 	sudo chown www-data:www-data /var/www/html/cgi-bin/switch_c.cgi
+run: all
+	export QUERY_STRING="switch_1=on"; export WIRINGPI_CODES=1; sudo -E ./switch_c.cgi
--- switch.c
+++ switch.c
@@ -9,6 +9,7 @@
 #define ERROR_1 "Variable name not found."
 #define ERROR_2 "Not a valid value."
 #define ERROR_3 "No variable provided."
+#define ERROR_4 "WiringPi could not be setup."
 
 #define VARIABLE_NAME "switch_1"
 #define ON "on"
@@ -17,66 +18,76 @@
 #define ERROR_CODE_1 1
 #define ERROR_CODE_2 2
 #define ERROR_CODE_3 3
+#define ERROR_CODE_4 4
 
 #define PIN_17 0
 
-void printHeaders(void);
-void printSuccess(void);
-void printFailure(int errorCode);
-void initWirePi();
+void printHeaders(int);
+int sprintSuccess(char *);
+int sprintFailure(int errorCode, char *);
+int initWirePi();
 
 int main(int argsc, char *argsv[])
 {
     char delim[2] = ",",
          subDelim[2] = "=",
-         *queryStr = getenv("QUERY_STRING");
+         *queryStr = getenv("QUERY_STRING"),
+         response[80];
      char *queryVariable,
           *qVarName,
           *qVarValue;
 
+    memset(response, '\0', sizeof(response));
+
     setlocale(LC_ALL, "");
-    printHeaders();
     if(queryStr == NULL){
-        printFailure(ERROR_CODE_3);
+        sprintFailure(ERROR_CODE_3, response);
     }else{
         queryVariable = strtok(queryStr, delim);
         qVarName = strtok(queryVariable, subDelim);
         qVarValue = strtok(NULL, subDelim);
 
         if(strcmp(qVarName, VARIABLE_NAME) != 0){
-            printFailure(ERROR_CODE_1);
+            sprintFailure(ERROR_CODE_1, response);
         }else{
-            initWirePi();
+            if(initWirePi() != 0){
+                sprintFailure(ERROR_CODE_4, response);
+            }
 
             if(strcmp(qVarValue, ON) == 0){
                 digitalWrite (PIN_17, HIGH);// On
-                printSuccess();
+                sprintSuccess(response);
             }else if(strcmp(qVarValue, OFF) == 0){
                 digitalWrite (PIN_17, LOW);// On
-                printSuccess();
+                sprintSuccess(response);
             }else{
-                printFailure(ERROR_CODE_2);
+                sprintFailure(ERROR_CODE_2, response);
             }
         }
     }
 
+    printHeaders(strlen(response));
+    printf("%s", response);
+
     return EXIT_SUCCESS;
 }
 
-void printHeaders(void)
+void printHeaders(int bodyLength)
 {
-    printf("%s\n", "Content-Type: application/json");
     printf("%s\n", "Cache-Control: no-cache");
     printf("%s\n", "pragma: no-cache");
+    printf("%s\n", "HTTP/1.0 200 OK");
+    printf("%s\n", "Content-Type: application/json");
+    printf("Content-Length: %d\n", bodyLength);
     printf("%s\n","");
 }
 
-void printSuccess(void)
+int sprintSuccess(char *response)
 {
-    printf("%s\n", SUCCESS);
+    return sprintf(response, SUCCESS);
 }
 
-void printFailure(int errorCode)
+int sprintFailure(int errorCode, char *response)
 {
     char *msg = "";
 
@@ -90,13 +101,18 @@ void printFailure(int errorCode)
         case ERROR_CODE_3:
             msg = ERROR_3;
             break;
+        case ERROR_CODE_4:
+            msg = ERROR_4;
+            break;
     }
 
-    printf(ERROR_MSG, msg);
+    return sprintf(response, ERROR_MSG, msg);
 }
 
-void initWirePi() 
+int initWirePi() 
 {
-    wiringPiSetup();
+    int status = wiringPiSetup();
     pinMode(PIN_17, OUTPUT);
+
+    return status;
 }
